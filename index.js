8b3f358a8e50dbb5c0378ab39d9c62958368df2b
launchpedia <129935912+launchpedia@users.noreply.github.com>
2023-04-06 18:56:34 +0530

    whattomeme2.0 update

---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+const API_KEY = 'keyAr6WRygtP75zKA';
+const BASE_ID = 'app84NjjgAoSumjQo';
+const TABLE_NAME = 'whattomeme';
+
+
+const generateMemeButton = document.querySelector('#generate-meme-btn');
+const memeContainer = document.querySelector('#meme-container');
+
+function randomFloor(h_len) {
+ return Math.floor(Math.random() * h_len)
+}
+
+generateMemeButton.addEventListener('click', () => {
+  fetch(`https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+     const randomMemeIndex =randomFloor(data.records.length);
+      const randomMeme = data.records[randomMemeIndex].fields;
+      const memeImageUrl = randomMeme['Meme URL'];
+      memeContainer.innerHTML = `
+        <img src="${memeImageUrl}" alt="Meme Image">
+      `;
+    })
+    .catch(error => console.error(error));
+});
