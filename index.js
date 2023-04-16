ba1050bd8dde0366a090e25061470a00c27ed9d0
launchpedia <129935912+launchpedia@users.noreply.github.com>
2023-04-16 18:55:53 +0530

    added logic to select memes from tables

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// index.js
 const API_KEY = 'keyAr6WRygtP75zKA';
 const BASE_ID = 'app84NjjgAoSumjQo';
-const TABLE_NAME = 'whattomeme';
-
 
 const generateMemeButton = document.querySelector('#generate-meme-btn');
 const memeContainer = document.querySelector('#meme-container');
 
-function randomFloor(h_len) {
- return Math.floor(Math.random() * h_len)
-}
-
+// Add event listener for Generate Meme button
 generateMemeButton.addEventListener('click', () => {
-  fetch(`https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}?api_key=${API_KEY}`)
+  const selectedTable = document.querySelector('input[name="table-selection"]:checked').value; // Get the selected table name from radio buttons
+  fetch(`https://api.airtable.com/v0/${BASE_ID}/${selectedTable}?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-     const randomMemeIndex =randomFloor(data.records.length);
+      const randomMemeIndex = Math.floor(Math.random() * data.records.length);
       const randomMeme = data.records[randomMemeIndex].fields;
       const memeImageUrl = randomMeme['Meme URL'];
       memeContainer.innerHTML = `
